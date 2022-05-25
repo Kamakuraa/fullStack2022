610e9fd367c424aeb00afcacb2f9f3817ee02806
alexbratu <a.cosmin.bratu@gmail.com>
2022-05-25 13:57:22 +0300

    new-add

---
+++ src/main/java/ro/fasttrackit/curs3/curs5/src/main/java/ro/fasttrackit/curs5/Curs5Application.java
@@ -0,0 +1,13 @@
+package ro.fasttrackit.curs5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curs5Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run (Curs5Application.class, args);
+  }
+
+}
---
+++ src/main/java/ro/fasttrackit/homeworkCurs3/ex2/Main.java
@@ -0,0 +1,4 @@
+package ro.fasttrackit.homeworkCurs3.ex2;
+
+public class Main {
+  }
